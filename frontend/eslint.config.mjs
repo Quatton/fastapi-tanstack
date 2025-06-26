4fb4b165fa88aac2610bb86659b87d81c1dc7663
Quatton <atomicatives@gmail.com>
2025-06-26 22:33:44 +0900

    chore: ignore generated files

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,6 +9,7 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     plugins: { js },
     extends: ["js/recommended"],
+    ignores: ["**/*.gen.ts", "**/_generated/**"],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
